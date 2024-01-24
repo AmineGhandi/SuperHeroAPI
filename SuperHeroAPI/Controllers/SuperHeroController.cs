2f17638872eec06a317809a704e6a6b8c242edde
Amine Ghandi <79575199+AmineGhandi@users.noreply.github.com>
2024-01-25 00:29:43 +0100

    Api fixation

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<Superhero>>> GetSuperHeroes()
+        {
+            return new List<Superhero>
+            {
+                new Superhero
+                {
+                        spName = "Spider Man",
+                        firstName = "Peter",
+                        lastName = "Parker",
+                        origin = "NYC"
+                }
+            };
+        }
+    }
+}
---
+++ SuperHeroAPI/Superhero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI
+{
+    public class Superhero
+    {
+        public int Id { get; set; }
+        public string spName { get; set; } = string.Empty;
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; } = string.Empty;
+        public string origin { get; set; } = string.Empty;
+
+    }
+}
