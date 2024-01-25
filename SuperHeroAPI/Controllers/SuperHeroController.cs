f3b14bda09c7c1c65c1a38b20653e5156e6eb5bd
Amine Ghandi <79575199+AmineGhandi@users.noreply.github.com>
2024-01-25 14:27:05 +0100

    CRUD API added, and working tested with Swagger

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -21,5 +21,43 @@ namespace SuperHeroAPI.Controllers
         {
             return Ok(await _Context.SuperHeroes.ToListAsync());
         }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Superhero>>> CreateSuperHeroes(Superhero hero)
+        {
+            _Context.SuperHeroes.Add(hero);
+            await _Context.SaveChangesAsync();
+
+            return Ok(await _Context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Superhero>>> UpdateSuperHeroes(Superhero hero)
+        {
+            var dbHero = await _Context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)
+                return BadRequest("Hero's not there");
+            
+
+            dbHero.spName = hero.spName;
+            dbHero.firstName = hero.firstName;
+            dbHero.lastName = hero.lastName;
+            dbHero.origin = hero.origin;
+
+            await _Context.SaveChangesAsync();
+            return Ok(await _Context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Superhero>>> DeleteSuperHeroes(int id)
+        {
+            var dbHero = await _Context.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
+                return BadRequest("Hero's not there");
+
+            _Context.SuperHeroes.Remove(dbHero); 
+            return Ok(await _Context.SuperHeroes.ToListAsync());
+        }
     }
 }
